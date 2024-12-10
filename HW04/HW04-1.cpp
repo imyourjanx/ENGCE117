53d6e1fd6529fb3633ce8f075634147e0fd3b40a
imyourjanx <imyourjanx99@gmail.com>
2024-12-10 14:51:54 +0700

    HW04-1.cpp

---
+++ HW04/HW04-1.cpp
@@ -0,0 +1,17 @@
+/*
+    จงแสดงผลตัวแปรทั้งหมดเรียงลำดับดังนี้ a b c d
+
+    Output:
+        10 20.0 Haha a
+*/
+
+#include <stdio.h>
+
+int main(){
+    int a = 10;
+    float b = 20;
+    char c[] = "Haha";
+    char d = 'a';
+
+printf( "%d %.1f %s %c", a, b, c, d); 
+}
